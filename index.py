813e6f6379cc7d2054eba593d437e3ac800e7c71
harjot-oberai <harjot.oberai@gmail.com>
2016-10-27 19:14:41 +0530

    Changes instrument

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 import pretty_midi
 import hashlib
+import random
 
-piano_c_chord = pretty_midi.PrettyMIDI()
-piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
-piano = pretty_midi.Instrument(program=piano_program)
+midi = pretty_midi.PrettyMIDI()
+instrument_program = pretty_midi.instrument_name_to_program('Music Box')
+instrument = pretty_midi.Instrument(program = instrument_program)
 
 input_string = raw_input("Type a string to convert to music: \n")
 
@@ -12,46 +13,51 @@ string = hash_object.hexdigest()
 
 def get_note_from_char(x):
 	if (x == '0'):
-		return 'C5'
-	elif (x == '1'):
-		return 'C#5'
-	elif (x == '2'):
-		return 'D5'
-	elif (x == '3'):
-		return 'D#5'
-	elif (x == '4'):
-		return 'E5'
-	elif (x == '5'):
-		return 'F5'
-	elif (x == '6'):
-		return 'F#5'
-	elif (x == '7'):
-		return 'G5'
-	elif (x == '8'):
-		return 'G#5'
-	elif (x == '9'):
-		return 'A5'
-	elif (x == 'a'):
-		return 'A#5'
-	elif (x == 'b'):
-		return 'B5'
-	elif (x == 'c'):
 		return 'C6'
-	elif (x == 'd'):
+	elif (x == '1'):
 		return 'C#6'
-	elif (x == 'e'):
+	elif (x == '2'):
 		return 'D6'
-	elif (x == 'f'):
+	elif (x == '3'):
 		return 'D#6'
+	elif (x == '4'):
+		return 'E6'
+	elif (x == '5'):
+		return 'F6'
+	elif (x == '6'):
+		return 'F#6'
+	elif (x == '7'):
+		return 'G6'
+	elif (x == '8'):
+		return 'G#6'
+	elif (x == '9'):
+		return 'A6'
+	elif (x == 'a'):
+		return 'A#6'
+	elif (x == 'b'):
+		return 'B6'
+	elif (x == 'c'):
+		return 'C7'
+	elif (x == 'd'):
+		return 'C#7'
+	elif (x == 'e'):
+		return 'D7'
+	elif (x == 'f'):
+		return 'D#7'
 
-i=0
+begin_time = 0
+play_time = 0
+delay_time = 0
+
 for c in string:
+	play_time = 0.4 + random.random()
+	delay_time = 0.1 + random.random() / 5
 	note_name = get_note_from_char(c)
 	note_number = pretty_midi.note_name_to_number(note_name)
-	note = pretty_midi.Note(velocity=100, pitch=note_number, start = i, end = i + 0.3)
-	piano.notes.append(note)
-	i = i + 0.5
+	note = pretty_midi.Note(velocity=100, pitch=note_number, start = begin_time, end = begin_time + play_time)
+	instrument.notes.append(note)
+	begin_time = begin_time + play_time + delay_time
 
-piano_c_chord.instruments.append(piano)
+midi.instruments.append(instrument)
 
-piano_c_chord.write(input_string + '.mid')
+midi.write(input_string + '.mid')
